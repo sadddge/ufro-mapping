f236f9af8c23743e95f3e3635b438d79d43920ec
sadddge <m.sanhueza22@ufromail.cl>
2024-10-04 21:03:27 -0300

    Update Sala.java
    Cambios a la clase Sala para testear si da problemas a la hora de hacer merge

--- backend/src/main/java/org/ufromap/Sala.java
+++ backend/src/main/java/org/ufromap/Sala.java
@@ -3,14 +3,26 @@ package org.ufromap;
 import java.util.List;
 
 public class Sala implements InfoMostrable, ClaseMostrable {
-    private long id;
-    private String nombre;
-    private List<Clase> clases;
+    private final long id;
+    private final String nombre;
+    private final List<Clase> clases;
 
     public Sala(long id, String nombre, List<Clase> clases) {
         this.id = id;
         this.nombre = nombre;
         this.clases = clases;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public List<Clase> getClases() {
+        return clases;
     }
 
     @Override
