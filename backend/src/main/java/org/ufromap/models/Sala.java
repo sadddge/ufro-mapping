3be41267e876b91bfab72e1d644f0b1252316059
sadddge <m.sanhueza22@ufromail.cl>
2024-10-09 19:23:38 -0300

    feat: add setters in Sala class

--- backend/src/main/java/org/ufromap/models/Sala.java
+++ backend/src/main/java/org/ufromap/models/Sala.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.ufromap.Clase;
 
 public class Sala{
-    private final long id;
-    private final String nombre;
-    private final List<Clase> clases;
+    private long id;
+    private String nombre;
+    private List<Clase> clases;
 
     public Sala(long id, String nombre, List<Clase> clases) {
         this.id = id;
@@ -27,4 +27,15 @@ public class Sala{
         return clases;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setClases(List<Clase> clases) {
+        this.clases = clases;
+    }
 }
