b0735b20a761619c4a9b033c383480f8fadb0579
BerAxz <bastiaxd@gmail.com>
2024-10-10 20:43:14 -0300

    Docs DatabaseConnection.java
    Se documenta la clase DatabaseConnection.java mediante Javadocs

--- backend/src/main/java/org/ufromap/config/DatabaseConnection.java
+++ backend/src/main/java/org/ufromap/config/DatabaseConnection.java
@@ -4,9 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Clase que gestiona la conexión a la base de datos utilizando las configuraciones
+ * proporcionadas por la clase {@link DatabaseConfig}. Implementa el patrón Singleton
+ * para asegurar que solo se mantenga una única instancia de la conexión a la base de datos.
+ */
 public class DatabaseConnection {
     private static Connection connection = null;
 
+    /**
+     * Obtiene la conexión a la base de datos. Si no existe una conexión activa,
+     * se crea una nueva utilizando las configuraciones proporcionadas en `config.properties`.
+     *
+     * @return La conexión activa a la base de datos.
+     */
     public static Connection getConnection() {
         if (connection == null) {
             try {
@@ -15,9 +26,7 @@ public class DatabaseConnection {
                 String username = config.getDatabaseUsername();
                 String password = config.getDatabasePassword();
                 String driver = config.getDatabaseDriver();
-
                 Class.forName(driver);
-                
                 connection = DriverManager.getConnection(url, username, password);
 
             } catch (ClassNotFoundException | SQLException e) {
