51a406e20ea714b623fd870601a28f10ba3e87b2
sadddge <m.sanhueza22@ufromail.cl>
2024-10-10 20:27:44 -0300

    docs(EdificioRepository): add javadocs

--- backend/src/main/java/org/ufromap/repositories/EdificioRepository.java
+++ backend/src/main/java/org/ufromap/repositories/EdificioRepository.java
@@ -11,26 +11,42 @@ import org.ufromap.config.DatabaseConnection;
 import org.ufromap.models.Edificio;
 import org.ufromap.models.Sala;
 
-
+/**
+ * Repositorio para manejar las operaciones relacionadas con la entidad {@link Edificio}.
+ * Provee métodos para realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar) sobre la tabla 'edificio'.
+ */
 public class EdificioRepository {
-    
+
     private final SalaRepository salaRepository;
 
+    /**
+     * Constructor por defecto que inicializa una instancia de {@link SalaRepository}.
+     */
     public EdificioRepository() {
         this.salaRepository = new SalaRepository();
     }
 
+    /**
+     * Constructor que permite inyectar una instancia de {@link SalaRepository}.
+     *
+     * @param salaRepository Instancia del repositorio de salas.
+     */
     public EdificioRepository(SalaRepository salaRepository) {
         this.salaRepository = salaRepository;
     }
 
+    /**
+     * Obtiene todos los edificios registrados en la base de datos.
+     *
+     * @return Una lista de objetos {@link Edificio} con los detalles de cada edificio y sus salas.
+     */
     public List<Edificio> getEdificios() {
         List<Edificio> edificios = new ArrayList<>();
         String query = "SELECT * FROM edificio";
         try (Connection connection = DatabaseConnection.getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
-            ResultSet resultSet = statement.executeQuery()){
-            
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+
             while (resultSet.next()) {
                 int id = resultSet.getInt("edificio_id");
                 String nombre = resultSet.getString("edificio");
@@ -43,19 +59,24 @@ public class EdificioRepository {
             }
 
         } catch (SQLException e) {
-
+            // Manejar la excepción adecuadamente
         }
         return edificios;
     }
 
+    /**
+     * Obtiene un edificio específico por su ID.
+     *
+     * @param id El ID del edificio a buscar.
+     * @return Un objeto {@link Edificio} con los detalles del edificio, o {@code null} si no se encuentra.
+     */
     public Edificio getEdificioById(int id) {
         Edificio edificio = null;
         String query = "SELECT * FROM edificio WHERE edificio_id = ?";
         try (Connection connection = DatabaseConnection.getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
-            ){
+             PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, id);
-            try (ResultSet resultSet = statement.executeQuery()){
+            try (ResultSet resultSet = statement.executeQuery()) {
                 if (resultSet.next()) {
                     String nombre = resultSet.getString("edificio");
                     String alias = resultSet.getString("alias");
@@ -66,21 +87,25 @@ public class EdificioRepository {
                     edificio = new Edificio(id, nombre, alias, latitud, longitud, tipo, salas);
                 }
             }
-            connection.close();
         } catch (SQLException e) {
-
+            // Manejar la excepción adecuadamente
         }
         return edificio;
     }
 
+    /**
+     * Busca un edificio por su nombre.
+     *
+     * @param nombre El nombre del edificio.
+     * @return Un objeto {@link Edificio} con los detalles del edificio, o {@code null} si no se encuentra.
+     */
     public Edificio getEdificioByNombre(String nombre) {
         Edificio edificio = null;
         String query = "SELECT * FROM edificio WHERE edificio = ?";
         try (Connection connection = DatabaseConnection.getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
-            ){
+             PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, nombre);
-            try (ResultSet resultSet = statement.executeQuery()){
+            try (ResultSet resultSet = statement.executeQuery()) {
                 if (resultSet.next()) {
                     int id = resultSet.getInt("edificio_id");
                     String alias = resultSet.getString("alias");
@@ -91,21 +116,25 @@ public class EdificioRepository {
                     edificio = new Edificio(id, nombre, alias, latitud, longitud, tipo, salas);
                 }
             }
-            connection.close();
         } catch (SQLException e) {
-
+            // Manejar la excepción adecuadamente
         }
         return edificio;
     }
 
+    /**
+     * Busca un edificio por su alias.
+     *
+     * @param alias El alias del edificio.
+     * @return Un objeto {@link Edificio} con los detalles del edificio, o {@code null} si no se encuentra.
+     */
     public Edificio getEdificioByAlias(String alias) {
         Edificio edificio = null;
         String query = "SELECT * FROM edificio WHERE alias = ?";
         try (Connection connection = DatabaseConnection.getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
-            ){
+             PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, alias);
-            try (ResultSet resultSet = statement.executeQuery()){
+            try (ResultSet resultSet = statement.executeQuery()) {
                 if (resultSet.next()) {
                     int id = resultSet.getInt("edificio_id");
                     String nombre = resultSet.getString("edificio");
@@ -116,21 +145,25 @@ public class EdificioRepository {
                     edificio = new Edificio(id, nombre, alias, latitud, longitud, tipo, salas);
                 }
             }
-            connection.close();
         } catch (SQLException e) {
-
+            // Manejar la excepción adecuadamente
         }
         return edificio;
     }
 
+    /**
+     * Busca un edificio por su tipo.
+     *
+     * @param tipo El tipo del edificio (por ejemplo, académico, administrativo, etc.).
+     * @return Un objeto {@link Edificio} con los detalles del edificio, o {@code null} si no se encuentra.
+     */
     public Edificio getEdificioByTipo(String tipo) {
         Edificio edificio = null;
         String query = "SELECT * FROM edificio WHERE tipo = ?";
         try (Connection connection = DatabaseConnection.getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
-            ){
+             PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, tipo);
-            try (ResultSet resultSet = statement.executeQuery()){
+            try (ResultSet resultSet = statement.executeQuery()) {
                 if (resultSet.next()) {
                     int id = resultSet.getInt("edificio_id");
                     String nombre = resultSet.getString("edificio");
@@ -141,35 +174,45 @@ public class EdificioRepository {
                     edificio = new Edificio(id, nombre, alias, latitud, longitud, tipo, salas);
                 }
             }
-            connection.close();
         } catch (SQLException e) {
-
+            // Manejar la excepción adecuadamente
         }
         return edificio;
     }
 
-
+    /**
+     * Agrega un nuevo edificio a la base de datos.
+     *
+     * @param edificio El objeto {@link Edificio} que contiene los datos del edificio a agregar.
+     * @return {@code true} si la operación fue exitosa, {@code false} si ocurrió algún error.
+     */
     public boolean addEdificio(Edificio edificio) {
         String query = "INSERT INTO edificio (edificio, alias, latitud, longitud, tipo) VALUES (?, ?, ?, ?, ?)";
         try (Connection connection = DatabaseConnection.getConnection();
-            PreparedStatement statement = connection.prepareStatement(query)){
+             PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, edificio.getNombre());
             statement.setString(2, edificio.getAlias());
             statement.setFloat(3, edificio.getLatitud());
             statement.setFloat(4, edificio.getLongitud());
             statement.setString(5, edificio.getTipo());
             statement.executeUpdate();
-            connection.close();
             return true;
         } catch (SQLException e) {
+            // Manejar la excepción adecuadamente
             return false;
         }
     }
 
+    /**
+     * Actualiza los datos de un edificio existente.
+     *
+     * @param edificio El objeto {@link Edificio} con los nuevos datos.
+     * @return {@code true} si la operación fue exitosa, {@code false} si ocurrió algún error.
+     */
     public boolean updateEdificio(Edificio edificio) {
         String query = "UPDATE edificio SET edificio = ?, alias = ?, latitud = ?, longitud = ?, tipo = ? WHERE edificio_id = ?";
         try (Connection connection = DatabaseConnection.getConnection();
-            PreparedStatement statement = connection.prepareStatement(query)){
+             PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, edificio.getNombre());
             statement.setString(2, edificio.getAlias());
             statement.setFloat(3, edificio.getLatitud());
@@ -177,22 +220,28 @@ public class EdificioRepository {
             statement.setString(5, edificio.getTipo());
             statement.setInt(6, edificio.getId());
             statement.executeUpdate();
-            connection.close();
             return true;
         } catch (SQLException e) {
+            // Manejar la excepción adecuadamente
             return false;
         }
     }
 
+    /**
+     * Elimina un edificio de la base de datos por su ID.
+     *
+     * @param id El ID del edificio a eliminar.
+     * @return {@code true} si la operación fue exitosa, {@code false} si ocurrió algún error.
+     */
     public boolean deleteEdificio(int id) {
         String query = "DELETE FROM edificio WHERE edificio_id = ?";
         try (Connection connection = DatabaseConnection.getConnection();
-            PreparedStatement statement = connection.prepareStatement(query)){
+             PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, id);
             statement.executeUpdate();
-            connection.close();
             return true;
         } catch (SQLException e) {
+            // Manejar la excepción adecuadamente
             return false;
         }
     }
