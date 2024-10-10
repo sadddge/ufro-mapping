27ba2f19f7ba8c16e6487e981a0cff60084a1472
sadddge <m.sanhueza22@ufromail.cl>
2024-10-10 14:38:07 -0300

    feat: add EdificioRepository class

---
+++ backend/src/main/java/org/ufromap/repositories/EdificioRepository.java
@@ -0,0 +1,19 @@
+package org.ufromap.repositories;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ufromap.config.DatabaseConnection;
+import org.ufromap.models.Edificio;
+
+
+public class EdificioRepository {
+    
+
+    
+
+}
