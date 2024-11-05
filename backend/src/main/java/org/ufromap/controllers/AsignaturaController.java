faa84b6962644d76f59eec8165b349997c476053
iiesteban <e.ortiz06@ufromail.cl>
2024-11-05 16:16:30 -0300

    Avance asignatura controller

--- backend/src/main/java/org/ufromap/controllers/AsignaturaController.java
+++ backend/src/main/java/org/ufromap/controllers/AsignaturaController.java
@@ -2,14 +2,26 @@ package org.ufromap.controllers;
 
 import org.ufromap.models.Asignatura;
 import org.ufromap.services.AsignaturaService;
+import org.ufromap.services.EdificioService;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * Controlador que maneja las solicitudes relacionadas con la entidad "Asignatura".
  * 
  */
-public class AsignaturaController {
+@WebServlet("/api/asignaturas/*")
+public class AsignaturaController extends BaseController {
 
     private final AsignaturaService asignaturaService;
+
+    public AsignaturaController() {
+        this.asignaturaService = new AsignaturaService();
+    }
 
     /**
      * Constructor que inicializa el controlador con el servicio de Asignatura.
@@ -19,5 +31,13 @@ public class AsignaturaController {
         this.asignaturaService = asignaturaService;
     }
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Map<String, String[]> params = request.getParameterMap();
+        String pathInfo = request.getPathInfo();
+        String[] pathParts = pathInfo == null ? new String[0] : pathInfo.trim().split("/");
+
+        System.out.println(pathInfo + " ola");
+    }
 
 }
