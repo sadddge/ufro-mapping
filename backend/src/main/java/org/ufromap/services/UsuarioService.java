ede95bde9496cee09aa77302401ac60499aa0413
Martin Muñoz <martin.andres.munoz.c@gmail.com>
2024-11-09 18:35:36 -0300

    chore: UsuarioService

--- backend/src/main/java/org/ufromap/services/UsuarioService.java
+++ backend/src/main/java/org/ufromap/services/UsuarioService.java
@@ -3,12 +3,12 @@ package org.ufromap.services;
 import org.ufromap.auth.Validator;
 import org.ufromap.exceptions.EntityNotFoundException;
 import org.ufromap.models.Usuario;
-import org.ufromap.repositories.AsignaturaRepository;
 import org.ufromap.repositories.UsuarioRepository;
 
 import java.util.List;
+import java.util.Map;
 
-public class UsuarioService {
+public class UsuarioService implements IService<Usuario> {
     private final UsuarioRepository usuarioRepository;
 
     public UsuarioService() {
@@ -19,10 +19,12 @@ public class UsuarioService {
         this.usuarioRepository = usuarioRepository;
     }
 
+    @Override
     public List<Usuario> findAll() {
         return usuarioRepository.findAll();
     }
 
+    @Override
     public Usuario findById(int id) {
         Usuario usuario = usuarioRepository.findById(id);
         if (usuario == null) {
@@ -31,18 +33,26 @@ public class UsuarioService {
         return usuarioRepository.findById(id);
     }
 
+    @Override
+    public List<Usuario> findByFilter(Map<String, Object> filters) throws EntityNotFoundException {
+        return List.of();
+    }
+
+    @Override
     public Usuario add(Usuario usuario) {
         Validator.validateEmail(usuario.getCorreo());
         Validator.validatePassword(usuario.getContrasenia());
         return usuarioRepository.add(usuario);
     }
 
+    @Override
     public Usuario update(Usuario usuario) {
         Validator.validateEmail(usuario.getCorreo());
         Validator.validatePassword(usuario.getContrasenia());
         return usuarioRepository.update(usuario);
     }
 
+    @Override
     public void delete(int id) {
         findById(id);
         usuarioRepository.delete(id);
