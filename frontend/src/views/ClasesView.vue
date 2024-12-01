ca10a7d43a5aeab08406a7a0f0aaaf07bf0aff36
sadddge <m.sanhueza22@ufromail.cl>
2024-11-30 23:07:47 -0300

    feat: add lecture page
    Add page for lectures with a table that contains all lectures and allows to add, edit and delete lectures.

---
+++ frontend/src/views/ClasesView.vue
@@ -0,0 +1,13 @@
+<template>
+    <MainLayout>
+        <div class="flex flex-col gap-4 m-auto items-end">
+          <LectureTable />
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+import LectureTable from '@/components/LectureTable.vue';
+import MainLayout from '@/layouts/MainLayout.vue';
+import CalendarSlot from "@/components/CalendarSlot.vue";
+</script>
