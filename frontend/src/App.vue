2a822ff6f4bcf39aab54c2b2c379476bc8559e40
sadddge <m.sanhueza22@ufromail.cl>
2024-11-30 22:18:26 -0300

    feat: add admin home page
    Add HomeView and MainLayout for future views.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="h-screen bg-zinc-900">
-    <n-config-provider :theme="darkTheme">
-      <RouterView/>
+  <div class="h-full w-screen bg-zinc-900">
+    <n-config-provider :theme="darkTheme" class="w-full h-full">
+      <n-dialog-provider>
+        <RouterView/>
+      </n-dialog-provider>
     </n-config-provider>
   </div>
 </template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <MainLayout>
+        <div class="flex flex-col items-center justify-center h-screen w-1/2 mx-auto">
+            <img src="../assets/images/ufro_logo.png">
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '@/layouts/MainLayout.vue';
+</script>
