8f7f989226df242815f0236b11928ece79a37d26
sadddge <m.sanhueza22@ufromail.cl>
2024-11-30 23:06:52 -0300

    feat: add classroom page
    Add page for classrooms with a table that contains all classrooms and allows to add, edit, delete and see the schedule of a classroom.

---
+++ frontend/src/views/SalasView.vue
@@ -0,0 +1,12 @@
+<template>
+    <MainLayout>
+        <div class="flex flex-row w-fit m-auto gap-10">
+            <ClassroomTable />
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+import ClassroomTable from '@/components/ClassroomTable.vue';
+import MainLayout from '@/layouts/MainLayout.vue';
+</script>
