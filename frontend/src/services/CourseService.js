b4f5ccd940c3105017b8f85d0a7323ab48cf9bc2
sadddge <m.sanhueza22@ufromail.cl>
2024-12-03 19:49:16 -0300

    fix: delete missplaced brackets on course service

--- frontend/src/services/CourseService.js
+++ frontend/src/services/CourseService.js
@@ -2,32 +2,32 @@ import axiosInstance from '../plugins/axios.js'
 class CourseService {
 
     async getCourses() {
-        const response = await axiosInstance().get('/asignaturas');
+        const response = await axiosInstance.get('/asignaturas');
         return response.data;
     }
 
     async getCourseById(id) {
-        const response = await axiosInstance().get(`/asignaturas/${id}`);
+        const response = await axiosInstance.get(`/asignaturas/${id}`);
         return response.data;
     }
 
     async createCourse(course) {
-        const response = await axiosInstance().post('/asignaturas', course);
+        const response = await axiosInstance.post('/asignaturas', course);
         return response.data;
     }
 
     async updateCourse(id, course) {
-        const response = await axiosInstance().put(`/asignaturas/${id}`, course);
+        const response = await axiosInstance.put(`/asignaturas/${id}`, course);
         return response.data;
     }
 
     async deleteCourse(id) {
-        const response = await axiosInstance().delete(`/asignaturas/${id}`);
+        const response = await axiosInstance.delete(`/asignaturas/${id}`);
         return response.data;
     }
 
     async getHorarioByCourseId(id) {
-        const response = await axiosInstance().get(`/asignaturas/${id}/horarios`);
+        const response = await axiosInstance.get(`/asignaturas/${id}/horarios`);
         return response.data;
     }
 
