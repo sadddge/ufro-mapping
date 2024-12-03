d7a489a06f04ee9d167c3d21f2020ecce2073c8c
sadddge <m.sanhueza22@ufromail.cl>
2024-12-03 12:40:55 -0300

    refactor: update services to use custom axios

--- frontend/src/services/ClassroomService.js
+++ frontend/src/services/ClassroomService.js
@@ -1,33 +1,33 @@
-import axios from 'axios';
+import axiosInstance from '../plugins/axios.js'
 class ClassroomService {
 
     async getClassrooms() {
-        const response = await axios.get('http://localhost:8080/ufro_mapping/api/salas');
+        const response = await axiosInstance.get('/salas');
         return response.data;
     }
 
     async getClassroomById(id) {
-        const response = await axios.get(`http://localhost:8080/ufro_mapping/api/salas/${id}`);
+        const response = await axiosInstance.get(`/salas/${id}`);
         return response.data;
     }
 
     async createClassroom(classroom) {
-        const response = await axios.post('http://localhost:8080/ufro_mapping/api/salas', classroom).catch((error) => alert(error.response.data.error));
+        const response = await axiosInstance.post('/salas', classroom);
         return response.data;
     }
 
     async updateClassroom(id, classroom) {
-        const response = await axios.put(`http://localhost:8080/ufro_mapping/api/salas/${id}`, classroom);
+        const response = await axiosInstance.put(`/salas/${id}`, classroom);
         return response.data;
     }
 
     async deleteClassroom(id) {
-        const response = await axios.delete(`http://localhost:8080/ufro_mapping/api/salas/${id}`);
+        const response = await axiosInstance.delete(`/salas/${id}`);
         return response.data;
     }
 
     async getHorarioByClassroomId(id) {
-        const response = await axios.get(`http://localhost:8080/ufro_mapping/api/salas/${id}/horario`)
+        const response = await axiosInstance.get(`/salas/${id}/horarios`);
         return response.data;
     }
 
