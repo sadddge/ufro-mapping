a56d90142d265598f9a69a3e7b0e32bfd9933c14
sadddge <m.sanhueza22@ufromail.cl>
2024-12-14 15:37:19 -0300

    feat: initialize Spring Boot project

---
+++ spring_backend/src/test/java/org/api/ufro_mapping/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api.ufro_mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
