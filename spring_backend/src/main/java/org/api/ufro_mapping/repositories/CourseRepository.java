3523eb97b98556bcbe49d3b64a02a7b7814d2a8d
sadddge <m.sanhueza22@ufromail.cl>
2024-12-17 05:35:47 -0300

    fix: delete unused import

--- spring_backend/src/main/java/org/api/ufro_mapping/repositories/CourseRepository.java
+++ spring_backend/src/main/java/org/api/ufro_mapping/repositories/CourseRepository.java
@@ -3,7 +3,5 @@ package org.api.ufro_mapping.repositories;
 import org.api.ufro_mapping.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CourseRepository extends JpaRepository<Course, Long> {
 }
