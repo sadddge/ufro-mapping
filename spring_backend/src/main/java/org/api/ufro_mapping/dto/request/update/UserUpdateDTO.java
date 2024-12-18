d883ee53fd9737aab4e9de92559653d0c1629ef2
sadddge <m.sanhueza22@ufromail.cl>
2024-12-18 14:45:05 -0300

    fix: change lenguage of error message on dto

--- spring_backend/src/main/java/org/api/ufro_mapping/dto/request/update/UserUpdateDTO.java
+++ spring_backend/src/main/java/org/api/ufro_mapping/dto/request/update/UserUpdateDTO.java
@@ -13,14 +13,14 @@ import org.api.ufro_mapping.models.Role;
 @NoArgsConstructor
 public class UserUpdateDTO {
     private Role role;
-    @Pattern(regexp = "^(?!\\s*$).+", message = "El campo no puede ser vacío, pero se permite null")
+    @Pattern(regexp = "^(?!\\s*$).+", message = "Name cannot be empty.")
     @Size(max = 20, message = "Name must be less than 20 characters.")
     private String name;
     @Email(message = "Invalid email format.")
-    @Pattern(regexp = "^(?!\\s*$).+", message = "El campo no puede ser vacío, pero se permite null")
+    @Pattern(regexp = "^(?!\\s*$).+", message = "Email cannot be empty.")
     @Size(max = 80, message = "The email must be less than 80 characters.")
     private String email;
-    @Pattern(regexp = "^(?!\\s*$).+", message = "El campo no puede ser vacío, pero se permite null")
+    @Pattern(regexp = "^(?!\\s*$).+", message = "Password cannot be empty.")
     @Size(max = 200, message = "The password must be less than 200 characters.")
     private String password;
 }
